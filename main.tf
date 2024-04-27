79797d9d74b402206e424214d505da11e7792f25
apoorvbajpai7 <apoorv.bajpai49@gmail.com>
2024-04-27 13:48:47 +0530

    all

--- main.tf
+++ main.tf
@@ -1,11 +1,58 @@
-// main.tf
+# main.tf
+
+# Azure provider configuration
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "my-resource-group"
-  location = "East US"
+# Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                     = "lneTask"
+  resource_group_name      = lne
+  location                 = "East US"
+  sku                      = "Basic"
 }
 
-// Define other Azure resources here (e.g., Azure Container Registry, App Service, etc.)
+# Azure App Service
+resource "azurerm_app_service" "app_service" {
+  name                = "myappservice"
+  location            = "East US"
+  resource_group_name = lne
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+
+  site_config {
+    # App Service configuration
+  }
+}
+
+# Azure Container Instance
+resource "azurerm_container_group" "container_group" {
+  name                = "lne"
+  location            = "East US"
+  resource_group_name = lne
+
+  container {
+    name   = "nginx"
+    image  = "nginx"
+    cpu    = "0.5"
+    memory = "1.5"
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  os_type    = "Linux"
+}
+
+# Azure PostgreSQL Flexible Server
+resource "azurerm_postgresql_server" "postgres_server" {
+  name                = "mypostgresqlserver"
+  location            = "East US"
+  resource_group_name = lne
+  sku_name            = "Standard_D2s_v3"
+  storage_profile     = "Standard_LRS"
+  version             = "12"
+  administrator_login          = "myadmin"
+  administrator_login_password = "Password123!"
+}
