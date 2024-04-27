025cb13ec62a374f39817742d99b5d6b47993aac
apoorvbajpai7 <apoorv.bajpai49@gmail.com>
2024-04-27 14:55:54 +0530

    ALL

--- variables.tf
+++ variables.tf
@@ -8,4 +8,9 @@ variable "resource_group_name" {
   type = string
 }
 
+variable "subscription_id" {
+  type    = string
+  # Optionally specify a default value if applicable
+  default = "YOUR_SUBSCRIPTION_ID_HERE"
+}
 // Define other variables as needed
